a3beff4caedd2891ca6c07b1d9f5391fc5985762
Rafey <59098398+rlom721@users.noreply.github.com>
2024-10-28 22:23:22 -0700

    Implement main gameplay function

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Rock, paper or scissors? ");
+    choice = choice.toLowerCase();
 
     while(choice != "rock" && choice != "paper" && choice != "scissors"
             && choice != "r" && choice != "p" && choice != "s") {
@@ -30,5 +31,39 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
+
+// Main gameplay code below
+
+let humanScore = 0,
+    computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock smashed scissors!");
+        humanScore += 1;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper wrapped and trashed rock!");
+        humanScore += 1;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors shredded the paper!");
+        humanScore += 1;
+    } else if (computerChoice == "rock" && humanChoice == "scissors") {
+        console.log("You lose! Rock smashed scissors!");
+        computerScore += 1;
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("You lose! Paper wrapped and trashed rock!");
+        computerScore += 1;
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("You lose! Scissors shredded the paper!");
+        computerScore += 1;
+    } else {
+        console.log("Gasp, it was a tie!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
