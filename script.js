863861465e96c85af5f0695ba2fd14b7ffef71c7
Rafey <59098398+rlom721@users.noreply.github.com>
2024-10-28 21:28:05 -0700

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let choice = "error"; 
+    let rand = Math.floor((Math.random() * 100)) % 3;
+    
+    // generate random number 0-2 to determine choice
+    switch(rand) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+    }          
+                
+    console.log(rand);
+    return choice;
+}
+
+function getHumanChoice() {
+    
+}
+
+console.log(getComputerChoice());
